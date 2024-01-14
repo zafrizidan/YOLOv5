e3dd9092623507b23c7e286d509d64466fca1395
zidan <86764692+zafrizidan@users.noreply.github.com>
2024-01-14 16:40:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import torch
 from PIL import Image
-import cv2
 import numpy as np
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Users/Zidan/Documents/yolov5/runs/train/yolov5s_results5/weights/best.pt', force_reload=True)
@@ -20,15 +19,6 @@ def detect_uploaded_image(uploaded_image):
     results = run_detection(image)
     return image, results
 
-# Function to perform inference on webcam feed
-def detect_webcam_feed():
-    video_capture = cv2.VideoCapture(0)
-    while True:
-        _, frame = video_capture.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = run_detection(frame)
-        st.image(frame, channels="RGB", use_column_width=True)
-        st.write(results.pandas().xyxy[0])  # Show detection results in a table
 
 # Streamlit file uploader for image upload
 uploaded_file = st.file_uploader("Choose an image..", type=["jpg", "jpeg", "png"])
@@ -40,23 +30,4 @@ if uploaded_file is not None:
     # Display uploaded image and detection results
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     if st.button('Detect Objects'):
-
-        # Display detection results in table form
-        st.subheader('Detection Results:')
-        detection_data = []
-        for detection in results.xyxy[0]:
-            class_index = int(detection[5])  # Class index
-            class_name = model.names[class_index]  # Get class name from model.names
-            confidence = round(float(detection[4]), 2)  # Confidence score
-            bounding_box = [round(float(coord), 2) for coord in detection[:4]]  # Bounding box coordinates
-            detection_data.append([class_name, confidence, bounding_box])
-
-    if detection_data:
-        st.table(detection_data)
-    else:
-        st.write("No objects detected.")
-
-# Streamlit webcam access
-if st.button('Use Webcam for real-time detecting'):
-    st.write('Accessing Webcam...')
-    detect_webcam_feed()
+        st.write(results.pandas().xyxy[0])  # Show detection results in a table
